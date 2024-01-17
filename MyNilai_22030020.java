8a0dcefafc2228e39111fe571a72507c10603938
KO1DS5 <121038924+KO1DS5@users.noreply.github.com>
2024-01-17 15:44:12 +0700

    Add files via upload

---
+++ MyNilai_22030020.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mynilai_22030020;
+
+/**
+ *
+ * @author user
+ */
+public class MyNilai_22030020 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
